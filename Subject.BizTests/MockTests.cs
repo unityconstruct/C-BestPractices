46fd795b229f2f96980c342e93637fe1c1e9f4f0
silosix <silosix@W7-DEV1>
2018-07-06 15:30:02 -0500

    Update Subject.BizTests with MockTest Class and MockTest1 method

---
+++ Subject.BizTests/MockTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Subject.BizTests
+{
+    [TestClass]
+    public class MockTests
+    {
+        [TestMethod]
+        public void MockTest1()
+        {
+            //Arrange
+            var expected = "Result Expected";
+            //Act
+            var result = "Result Expected";
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
